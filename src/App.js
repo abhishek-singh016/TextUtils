605a39850d81882a353874f294941bbe29e27294
Abhishek Singh <abhishekps5171@gmail.com>
2025-02-22 23:45:08 +0530

    new Updates

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+// import About from './components/About';
 
 function App() {
   return (
     <div>
-       {/* <Navbar title="TextUtils" about="about"/> */}
-       <Navbar />
+       <Navbar title="TextUtils" about="About"/>
        <div className="container my-3">
        <TextForm heading ="Type below"/>
-
+       {/* <About /> */}
        </div>
     </div>
  
--- src/components/TextForm.jsx
+++ src/components/TextForm.jsx
@@ -1,9 +1,12 @@
-import React, {useState} from 'react'
+import React, {useState} from 'react' // useState ka use krna aaj sikha maine
 
 function TextForm(props){
 
     const handleUpperCase = ()=>{
         setText(text.toUpperCase());
+    }
+    const handleLowerCase =()=>{
+        setText(text.toLowerCase());
     }
 
     const handleChange = (event)=>{
@@ -11,16 +14,39 @@ function TextForm(props){
         setText(event.target.value);
     }
 
-    const[text, setText] = useState("Enter your text here!!");
+    const[text, setText] = useState("");
+
+
+    const handleSpeak = () => {
+        const speech = new SpeechSynthesisUtterance(text);
+        speech.lang = "en-US"; // You can change the language
+        speech.rate = 1; // Adjust speed (0.5 - 2)
+        speech.pitch = 1; // Adjust pitch (0 - 2)
+        window.speechSynthesis.speak(speech);
+    };
+
+    function handleClear(){
+        setText('');
+    }
 
     return(
     <div>
-        <div className="mb-3">
+        <div className="container mb-3">
             <h1>{props.heading}</h1>
-            <textarea className="form-control" value={text} onChange={handleChange} id="typeBox" rows="8"></textarea>
+            <textarea className="form-control" placeholder='Enter Your Text Here!!' value={text} onChange={handleChange} id="typeBox" rows="8"></textarea>
             <div>
-                <button className="btn btn-primary" onClick={handleUpperCase}> Convert To Uppercase</button>
+                <button className="btn btn-primary mx-1 my-1" onClick={handleUpperCase}> Convert To Uppercase</button>
+                <button className="btn btn-primary mx-1 my-1" onClick={handleLowerCase}> Convert To Lowercase</button>
+                <button className="btn btn-primary mx-1 my-1"  onClick={handleSpeak}>Speak</button>
+                <button className="btn btn-primary mx-1 my-1"  onClick={handleClear}>Clear</button>
+  
             </div>
+            <div className="container my-3">
+                <h2>Your text summary</h2>
+                <p>{text.split(" ").length - 1} words and {text.length} characters</p>
+                <p>{0.008 * text.split(" ").length} Minutes it'll take to read!!</p>
+            </div>
+
         </div>
     </div>
     )
