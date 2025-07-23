e023357268c3a7cc2b286084502c30b5f598037b
Abhishek Singh <abhishekps5171@gmail.com>
2025-07-23 16:13:39 +0530

    Mode switch Enabled

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 
+import React, { useState } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 // import About from './components/About';
 
 function App() {
+  const [mode, setMode] = useState('light');
+
+  const toggleMode = () =>{
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = '#03224e';
+    }else{
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+    }
+  }
   return (
     <div>
-       <Navbar title="TextUtils" about="About"/>
+       {/* <Navbar title="TextUtils" about="About"/> */}
+       <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
        <div className="container my-3">
-       <TextForm heading ="Type below"/>
+       <TextForm heading ="Type below" mode={mode}/>
        {/* <About /> */}
        </div>
     </div>
--- src/components/TextForm.jsx
+++ src/components/TextForm.jsx
@@ -29,19 +29,33 @@ function TextForm(props){
         setText('');
     }
 
+    const handleCopy = () =>{
+        let text = document.getElementById("typeBox");
+        text.select();
+        navigator.clipboard.writeText(text.value);
+    }
+
+    const handleExtraSpaces = () =>{
+        let newText = text.split(/[ ]+/);
+        setText(newText.join(" "));
+    }
+
+
     return(
     <div>
-        <div className="container mb-3">
+        <div className="container mb-3" style={{color: props.mode === 'dark' ?'white' : '#03224e'}}>
             <h1>{props.heading}</h1>
-            <textarea className="form-control" placeholder='Enter Your Text Here!!' value={text} onChange={handleChange} id="typeBox" rows="8"></textarea>
+            <textarea className="form-control" placeholder='Enter Your Text Here!!' value={text} onChange={handleChange} style={{backgroundColor: props.mode === 'dark' ?'grey' : 'white', color: props.mode === 'dark' ?'white' : '#03224e'}} id="typeBox" rows="8"></textarea>
             <div>
                 <button className="btn btn-primary mx-1 my-1" onClick={handleUpperCase}> Convert To Uppercase</button>
                 <button className="btn btn-primary mx-1 my-1" onClick={handleLowerCase}> Convert To Lowercase</button>
                 <button className="btn btn-primary mx-1 my-1"  onClick={handleSpeak}>Speak</button>
                 <button className="btn btn-primary mx-1 my-1"  onClick={handleClear}>Clear</button>
-  
+                <button className="btn btn-primary mx-1 my-1"  onClick={handleCopy}>Copy Text</button>
+                <button className="btn btn-primary mx-1 my-1"  onClick={handleExtraSpaces}>Remove Extra Spaces</button>
+
             </div>
-            <div className="container my-3">
+            <div className="container my-3" style={{color: props.mode === 'dark' ?'white' : '#03224e'}}>
                 <h2>Your text summary</h2>
                 <p>{text.split(" ").length - 1} words and {text.length} characters</p>
                 <p>{0.008 * text.split(" ").length} Minutes it'll take to read!!</p>
